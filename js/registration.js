4bd913ab58382a2dbbbc217c4f12a2f4cb9ecd6a
Tina Fallahi <fallahi.tina@gmail.com>
2014-02-14 11:39:34 +0000

    Finished registration front-end

--- js/registration.js
+++ js/registration.js
@@ -1,11 +1,11 @@
 $(function() {
-	var locationls = [];
-	var educationls = [];
-	var workls = [];
-
 	$('#locationform').hide();
 	$('#educationform').hide();
 	$('#workform').hide();
+	
+	var locationls = [];
+	var educationls = [];
+	var workls = [];
 
 	$('#location').on('click', function(e) {
 		e.preventDefault();
@@ -19,7 +19,6 @@ $(function() {
 		var loctype = $('#loctype').val();
 		var loc = locid.concat(', ', loctype);
 		locationls.push(loc);
-		alert(locationls);
 		$('#locationform').hide();
 		$('#locationlist').empty();
 		for(var i=0; i<locationls.length; i++) {
@@ -61,9 +60,43 @@ $(function() {
 		var wor = workid.concat(', ', wstart,', ', wend, ', ', wposition);
 		workls.push(wor);
 		$('#workform').hide();
-		$("#worklist").empty();
+		$('#worklist').empty();
 		for(var i=0; i<workls.length; i++) {
-			$("#worklist").append("<li>"+workls[i]+"</li>");
+			$('#worklist').append('<li>'+workls[i]+'</li>');
 		}
+	});
+
+	$('#register').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		// name all of it must be there
+		var fname = $('#fname').val(); 
+		var lname = $('#lname').val();
+		// Check if already exists
+		var email = $('#email').val();
+		// Check with confirmation
+		// Check some patterns for the password
+		var password = $('#password').val();
+		var spassword = $('#spassword').val();
+		// make sure not empty
+		var bday = $('#birthday').val();
+		// make sure not empty
+		var gender = $('input:radio[name=gender]').val();
+		var aboutme = $('#aboutme').val();
+
+		$.ajax({
+      		url: 'helpers/echo.php',
+      		type: 'post',
+      		data: {'fname': fname, 'lname': lname, 'email': email,
+      		 'password': password, 'bday': bday, 'gender': gender, 'aboutme': aboutme, 
+      		 'locationls': locationls, 'educationls': educationls,'workls': workls},
+        	success: function(data) {
+          		console.log(data);
+        	},
+        	error: function(xhr, desc, err) {
+          		console.log(xhr);
+          		console.log("Details: " + desc + "\nError:" + err);
+        	}
+    	});
 	});
 });
