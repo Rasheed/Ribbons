84ad6660b74d8793a34b2d649eb8e043bad2b112
Tina Fallahi <fallahi.tina@gmail.com>
2014-02-24 10:47:48 +0000

    Starting User Profile

--- js/registration.js
+++ js/registration.js
@@ -6,6 +6,16 @@ $(function() {
 	var locationls = [];
 	var educationls = [];
 	var workls = [];
+	var fname = '';
+	var lname = '';
+	var email = '';
+	var password = '';
+	var bday = '';
+	var gender = '';
+	var aboutme = '';
+
+	var emailreg = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+	var passreg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/; 
 
 	$('#location').on('click', function(e) {
 		e.preventDefault();
@@ -66,37 +76,76 @@ $(function() {
 		}
 	});
 
+	function checkRegisterFields() {
+		fname = $('#fname').val(); 
+		if(fname=='') {
+			alert('You must include a first name.');
+			return false;
+		}
+		lname = $('#lname').val();
+		if(lname=='') {
+			alert('You must include a last name.');
+			return false;
+		}
+		// Check if already exists
+		email = $('#email').val();
+		if(email=='') {
+			alert('You must include an email.');
+			return false;
+		}
+		if(!emailreg.test(email)) {
+			alert('You must include a valid email.');
+			return false;
+		}
+		// Check some patterns for the password
+		password = $('#password').val();
+		spassword = $('#spassword').val();
+		if(password.length < 6) {
+			alert('Password must be at least 6 characters long.');
+			return false;
+		}
+    	if (!passreg.test(password)) {
+        	alert('Password must contain at least one number, one lowercase and one uppercase letter.');
+        	return false;
+    	}
+		if(password!=spassword) {
+			alert('The passwords entered do not match.');
+			return false;
+		}
+		bday = $('#birthday').val();
+		if(bday=='') {
+			alert('You must include a birth date.');
+			return false;
+		}
+		gender = $('input[name=gender]:checked').val();
+		if(typeof gender === "undefined") {
+			alert('You must pick a gender.');
+			return false;
+		}
+		aboutme = $('#aboutme').val();
+		return true;
+	}
+
 	$('#register').on('click', function(e) {
 		e.preventDefault();
 		e.stopPropagation();
-		// name all of it must be there
-		var fname = $('#fname').val(); 
-		var lname = $('#lname').val();
-		// Check if already exists
-		var email = $('#email').val();
-		// Check with confirmation
-		// Check some patterns for the password
-		var password = $('#password').val();
-		var spassword = $('#spassword').val();
-		// make sure not empty
-		var bday = $('#birthday').val();
-		// make sure not empty
-		var gender = $('input:radio[name=gender]').val();
-		var aboutme = $('#aboutme').val();
 
-		$.ajax({
-      		url: 'helpers/echo.php',
-      		type: 'post',
-      		data: {'fname': fname, 'lname': lname, 'email': email,
-      		 'password': password, 'bday': bday, 'gender': gender, 'aboutme': aboutme, 
-      		 'locationls': locationls, 'educationls': educationls,'workls': workls},
-        	success: function(data) {
-          		console.log(data);
-        	},
-        	error: function(xhr, desc, err) {
-          		console.log(xhr);
-          		console.log("Details: " + desc + "\nError:" + err);
-        	}
-    	});
+		if(checkRegisterFields()){
+			$.ajax({
+      			url: 'helpers/echo.php',
+      			type: 'post',
+      			data: {'fname': fname, 'lname': lname, 'email': email,
+      		 	'password': password, 'bday': bday, 'gender': gender, 'aboutme': aboutme, 
+      		 	'locationls': locationls, 'educationls': educationls,'workls': workls},
+        		success: function(data) {
+          			console.log(data);
+          			alert('User registered!');
+        		},
+        		error: function(xhr, desc, err) {
+          			console.log(xhr);
+          			console.log("Details: " + desc + "\nError:" + err);
+        		}
+    		});
+    	}
 	});
 });
