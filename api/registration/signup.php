c6e9076d57eab25bdeccce0cbc239487165d78e7
rwihaib <rasheedwihaib@gmail.com>
2014-02-24 10:50:23 +0000

    get posts and signup update

--- api/registration/signup.php
+++ api/registration/signup.php
@@ -11,20 +11,12 @@
 		  $gender = $_POST['gender'];		  
 		  $birthday = $_POST['birthday'];
 		  //TODO field checks
-		  $sql_select = "SELECT Id FROM users WHERE Email = ?";
-		  $stmt = $conn->prepare($sql_select);
-		  $stmt->execute(array($email));
-		  $users = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-		  if(count($users)>0){
-			echo "user exists";
-			return;
-		  } else { 
-			$sql_insert = "INSERT INTO users (Email,Password,FirstName,LastName,Gender,Birthday) VALUES (?,?,?,?,?,?);";
-			$stmt = $conn->prepare($sql_insert);
-			$stmt->execute(array($email, $password, $first_name, $last_name, $gender, $birthday));
-			echo $stmt;
-			return;
-		  }
+		$sql_insert = "INSERT INTO users (Email,Password,FirstName,LastName,Gender,Birthday) VALUES (?,?,?,?,?,?);";
+				$stmt = $conn->prepare($sql_insert);
+				$stmt->execute(array($email, $password, $first_name, $last_name, $gender, $birthday));
+				echo $stmt;
+				return;
+		}
 		  echo json_encode($users);
 		}
 		else{
