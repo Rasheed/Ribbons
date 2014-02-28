1205a158ce625cef70a6736f3766fbef7572b022
rwihaib <rasheedwihaib@gmail.com>
2014-02-28 12:17:54 +0000

    signup revert and get friends and posts API extended

---
+++ curl/getfriendstest.php
@@ -0,0 +1,8 @@
+<?php                                                                               
+	$ch = curl_init('http://localhost/api/friends/get_friends_list.php?id=1');                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                     
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                                                                 
+	 
+	$result = curl_exec($ch);
+	echo $result;
+?>
