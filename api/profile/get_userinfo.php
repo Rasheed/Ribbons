3e4fdf99071192ee1b267730e99062f4cffc22f6
rwihaib <rasheedwihaib@gmail.com>
2014-02-28 18:16:38 +0000

    get user info updated

--- api/profile/get_userinfo.php
+++ api/profile/get_userinfo.php
@@ -5,7 +5,9 @@
 	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 		if (isset($_GET['id'])) {
 			$id = $_GET['id'];
-			$sql_select = "SELECT * FROM users WHERE Id = ?";
+			$sql_select = "SELECT u.Id, u.FirstName, u.LastName, u.Gender, u.CurrentRibbonPhoto, u.CurrentProfilePhoto, w.Name, w.Position, e.Name, e.StartDate, e.EndDate, e.Course
+							FROM users u, user_locations ul, workplace w, education e
+							WHERE u.Id = ? AND u.UserLocationId = ul.UserLocationId AND w.WorkplaceId = u.WorkplaceId AND e.EducationId = u.EducationId;";
 			$stmt = $conn->prepare($sql_select);
 			$stmt->execute(array($id));
 			$return = $stmt->fetchAll(PDO::FETCH_ASSOC); 
