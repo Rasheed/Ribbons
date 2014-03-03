1b39131719d47818e66741ce8eb120731f59837b
rwihaib <rasheedwihaib@gmail.com>
2014-03-03 13:49:13 +0000

    login update/ home design start

--- api/login/check_login.php
+++ api/login/check_login.php
@@ -5,17 +5,14 @@
 	if(isset($_POST['username'], $_POST['password'])) {
 	  $username = $_POST['username'];
 	  $password = $_POST['password'];
-	  $sql_select = "SELECT Id, UserLocationId, WorkplaceId, EducationId FROM users WHERE Email = ? AND Password = ?";
+	  $sql_select = "SELECT Id, UserLocationId, WorkplaceId, EducationId, CurrentRibbonPhoto, CurrentProfilePhoto FROM users WHERE Email = ? AND Password = ?";
 	  $stmt = $conn->prepare($sql_select);
 	  $stmt->execute(array($username, $password));
 	  $users = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 	  if(count($users) == 1) {
 		$data = array("isUser" => "true");   
 		$data['Id'] = $users[0]['Id'];
-		$data['UserLocationId'] = $users[0]['UserLocationId'];
-		$data['WorkplaceId'] = $users[0]['WorkplaceId'];
-		$data['EducationId'] = $users[0]['EducationId'];
-		$data['isComplete'] = isset($users[0]['UserLocationId'], $users[0]['WorkplaceId'], $users[0]['EducationId']);
+		$data['isComplete'] = isset($users[0]['UserLocationId'], $users[0]['WorkplaceId'], $users[0]['EducationId'], $users[0]['CurrentRibbonPhoto'], $users[0]['CurrentProfilePhoto']);
 		echo json_encode($data);
 		} else {
 			$data = array("isUser" => "false");                                                                    
