281b2d2c7b1cb7963f3e29bc62e461704cd64aa7
Rasheed Wihaib <rasheedwihaib@gmail.com>
2014-03-22 00:46:19 +0000

    friend profile update

--- js/friend_profile.js
+++ js/friend_profile.js
@@ -97,4 +97,60 @@ $(function() {
 					});
             e.target.setAttribute('disabled', 'disabled');
         });
+    $.ajax({
+        async: false,
+        url: 'api/friends/get_friends.php',
+        type: 'GET',
+        data: {
+            'id': viewId
+        },
+        success: function(data) {
+            console.log(data);
+            var friends = JSON.parse(data);
+            var friendnumber = $('<div/>', {
+				html: ""
+            });
+            $("#main-wrapper").append(friendnumber);
+            for (var i = 0; i < friends.length; i++) {
+                console.log(friends[i]);
+                var bodytext = $('<p>', {
+                    html: 'You are friends' //friends[i].toString()
+                });
+                var email = $('<h4>', {
+                    html: friends[i].Email
+                });
+                var name = $('<h3>', {
+                    html: friends[i].FirstName + " " + friends[i].LastName
+                });
+                var ribbonhovertext = $('<div/>', {
+                    class: 'ribbon-hover-text'
+                });
+                var ribbon = $('<div/>', {
+                    id: friends[i].UserId2.toString(),
+                    class: 'ribbon',
+                    style: "background-image:url('"+friends[i].RibbonPicture+"'); background-size: cover;"
+                });
+                ribbonhovertext.append(name);
+                ribbonhovertext.append(email);
+                ribbonhovertext.append(bodytext);
+                ribbon.append(ribbonhovertext);
+                var profileimage = $('<div/>', {
+                    class: 'profile-image ribbon-text',
+                    style: "background-image:url('"+friends[i].ProfilePicture+"'); background-size: cover; background-repeat:no-repeat;"
+                });
+                ribbon.append(profileimage);
+                ribbon.click(function(e) {
+					sessionStorage.setItem('viewId', e.target.getAttribute('id'));
+					console.log(sessionStorage.getItem('viewId'));
+					window.location.href = "/viewprofile.html";	
+                });
+                $("#main-wrapper").append(ribbon);
+            }
+        },
+        error: function(xhr, desc, err) {
+            console.log(xhr);
+            console.log('Details: ' + desc + '\nError:' + err);
+        }
+    });
+	
 });   
