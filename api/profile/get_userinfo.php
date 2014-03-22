c98cc52cd4b2ddf02ea42cf871c434bd7ec94321
Tina Fallahi <fallahi.tina@gmail.com>
2014-03-22 00:41:16 +0000

    user_profile fixes

--- api/profile/get_userinfo.php
+++ api/profile/get_userinfo.php
@@ -40,9 +40,7 @@
 				$loc_stmt = $conn->prepare($loc_select);
 				$loc_stmt->execute(array($locations[$i]));
 				$loc_return = $loc_stmt->fetchAll(PDO::FETCH_ASSOC);
-				//if(count($loc_return)>0){
-					$loc_result[$i] = $loc_return[0]["Name"].", ".$loc_return[0]["Status"];
-				//}
+				$loc_result[$i] = $loc_return[0]["Status"].": ".$loc_return[0]["Name"];
 			}
 			$data["LocationIds"] = $loc_result;
 
@@ -53,9 +51,7 @@
 				$edu_stmt = $conn->prepare($edu_select);
 				$edu_stmt->execute(array($educations[$i]));
 				$edu_return = $edu_stmt->fetchAll(PDO::FETCH_ASSOC);
-				//if(count($edu_return)>0){
-				$edu_result[$i] = $edu_return[0]["Name"].", ".$edu_return[0]["StartDate"].", ".$edu_return[0]["EndDate"].", ".$edu_return[0]["Course"];
-				//}
+				$edu_result[$i] = "Studied ".$edu_return[0]["Course"]." at ".$edu_return[0]["Name"].", from ".$edu_return[0]["StartDate"].", until ".$edu_return[0]["EndDate"];
 			}
 			$data["EducationIds"] = $edu_result;
 
@@ -66,9 +62,7 @@
 				$work_stmt = $conn->prepare($work_select);
 				$work_stmt->execute(array($works[$i]));
 				$work_return = $work_stmt->fetchAll(PDO::FETCH_ASSOC);
-				//if(count($edu_return)>0){
-					$work_result[$i] = $work_return[0]["Name"].", ".$work_return[0]["Position"].", ".$work_return[0]["StartDate"].", ".$work_return[0]["EndDate"];
-				//}
+					$work_result[$i] = "Worked as ".$work_return[0]["Position"]." at ".$work_return[0]["Name"]." , from ".$work_return[0]["StartDate"]." , until ".$work_return[0]["EndDate"];
 			}
 			$data["WorkIds"] = $work_result;
 			
