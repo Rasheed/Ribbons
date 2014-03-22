922837610771445d7f0d2bf15e8a37643076303e
Rasheed Wihaib <rasheedwihaib@gmail.com>
2014-03-22 00:49:31 +0000

    commit for revert

--- js/blogpost.js
+++ js/blogpost.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    var comment = $('#blogpost textarea'),
+    var comment = $('#post'),
         counter = '',
         counterValue = 140, //change this to set the max character count
         minCommentLength = 10, //set minimum comment length
         $commentValue = comment.val(),
         $commentLength = $commentValue.length,
-        submitButton = $('#blogpost input[type=submit]').hide();
+        submitButton = $('#add_post').hide();
   
-    $('form').prepend('<span class="counter"></span>').append('<p class="info">Min length: <span></span></p>');
+    $('#blogpost').prepend('<span class="counter"></span>').append('<p class="info">Min length: <span></span></p>');
     counter = $('span.counter');
     counter.html(counterValue); //display your set max length
     comment.attr('maxlength', counterValue); //apply max length to textarea
-    $('form').find('p.info > span').html(minCommentLength);
+    $('#blogpost').find('p.info > span').html(minCommentLength);
     // everytime a key is pressed inside the textarea, update counter
     comment.keyup(function () {
       var $this = $(this);
@@ -22,21 +22,5 @@ $(document).ready(function () {
       } else {
         submitButton.fadeOut(200);
       }
-    });
-	
-	var showForm = false;
-	$("#form").hide();
-	$("#pictureforms").hide();
-	$("#edit-ribbon").click(function(){
-	if(showForm){
-		$("#form").hide();
-		$("#pictureforms").hide();
-		showForm = false;
-		} else {
-			$("#form").show();
-			$("#pictureforms").show();
-			showForm = true;
-		}
 	});
-
 });
